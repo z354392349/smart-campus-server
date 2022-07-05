8d2bd70cb4b6080178aca7ca704f0049ae9a2115
zhangjiawei <48988840@qq.com>
2022-07-05 18:03:27 +0800

    接口首次调通

--- router/student.go
+++ router/student.go
@@ -7,14 +7,14 @@ import (
 )
 
 func InitStudentRouter(Router *gin.RouterGroup) {
-	ExcelRouter := Router.Group("excel")
+	ExcelRouter := Router.Group("student")
 	{
 		// ExcelRouter.POST("/importExcel", v1.ImportExcel)          // 导入Excel
 		// ExcelRouter.GET("/loadExcel", v1.LoadExcel)               // 加载Excel数据
 		// ExcelRouter.POST("/exportExcel", v1.ExportExcel)          // 导出Excel
 		// ExcelRouter.GET("/downloadTemplate", v1.DownloadTemplate) // 下载模板文件
 
-		ExcelRouter.GET("/getStudentList", v1.LoadExcel) // 获取学生信息
+		ExcelRouter.GET("/getStudentList", v1.GetStudentList) // 获取学生信息
 
 	}
 }

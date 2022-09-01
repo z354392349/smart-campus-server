ab8cda85a29a832d631215865e2759cd0a28b537
zhangjiawei <48988840@qq.com>
2022-09-01 18:22:36 +0800

    数据库备份

--- model/student.go
+++ model/student.go
@@ -4,22 +4,6 @@ import (
 	"gin-vue-admin/global"
 )
 
-// TODO:学生表要加一个 状态， 记录毕业，还是离校，还是在上学
-
-//type Student struct {
-//	global.GVA_MODEL
-//	Name        string `json:"name" gorm:"comment:学生名称"`
-//	Birthday    int    `json:"birthday" gorm:"comment:出生日期"`
-//	Sex         int    `json:"sex" gorm:"comment:性别 1表示男，2表示女。"`
-//	Telephone   string `json:"telephone" gorm:"comment:家长电话"`
-//	GradeID     uint   `json:"gradeID" gorm:"comment:外键年级ID;" `
-//	Grade       *Grade `json:"grade" gorm:"foreignKey:GradeID;"`
-//	ClassID     uint   `json:"classID" gorm:"comment:外键班级ID;" `
-//	Class       *Class `json:"class" gorm:"foreignKey:ClassID;"`
-//	SysUserID   uint   `json:"sysUserID" gorm:"comment:用户UUID"`
-//	Description string `json:"description" gorm:"comment:描述"`
-//}
-
 type Student struct {
 	global.GVA_MODEL
 	Name        string `json:"name" form:"name" gorm:"comment:学生名称"`

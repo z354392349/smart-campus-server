ee3fff4f5fed5649cd140726b52bc9b45c9bf7d9
zhangjiawei <48988840@qq.com>
2022-09-05 00:33:47 +0800

    数据整理

--- service/dashboard.go
+++ service/dashboard.go
@@ -15,9 +15,11 @@ import (
 // @Date:2022/08/08 21:02:59
 
 func GetDashboardCensusNum() (err error, carAccess int64, peopleAccess int64, teacherCensus response.TeacherCensus, studentCensus response.StudentCensus) {
+	var startTime int = 1667318400
+	var endTime int = 1667404799
 
 	// 获取当日车辆通行数量
-	if err = global.GVA_DB.Model(&model.CarAccess{}).Where("time >=  ? and time <= ? ", 1667318400, 1667404799).Count(&carAccess).Error; err != nil {
+	if err = global.GVA_DB.Model(&model.CarAccess{}).Where("time >=  ? and time <= ? ", startTime, endTime).Count(&carAccess).Error; err != nil {
 		err = errors.New("车辆通行数量获取失败")
 		return
 	}
@@ -25,11 +27,11 @@ func GetDashboardCensusNum() (err error, carAccess int64, peopleAccess int64, te
 	// 获取当日人员通行数量
 	var teacherAccess int64
 	var studentAccess int64
-	if err = global.GVA_DB.Model(&model.TeacherAccess{}).Where("time >=  ? and time <= ? ", 1667318400, 1667404799).Count(&teacherAccess).Error; err != nil {
+	if err = global.GVA_DB.Model(&model.TeacherAccess{}).Where("time >=  ? and time <= ? ", startTime, endTime).Count(&teacherAccess).Error; err != nil {
 		err = errors.New("教师通行数量获取失败")
 		return
 	}
-	if err = global.GVA_DB.Model(&model.StudentAccess{}).Where("time >=  ? and time <= ? ", 1667318400, 1667404799).Count(&studentAccess).Error; err != nil {
+	if err = global.GVA_DB.Model(&model.StudentAccess{}).Where("time >=  ? and time <= ? ", startTime, endTime).Count(&studentAccess).Error; err != nil {
 		err = errors.New("学生通行数量获取失败")
 		return
 	}
@@ -37,7 +39,7 @@ func GetDashboardCensusNum() (err error, carAccess int64, peopleAccess int64, te
 
 	// 获取教师考勤
 	selectSql := "select * from (select teacher_id, time  from teacher_accesses union all select teacher_id, time  from car_accesses) tb where time >=  ? and time <= ?  group by  tb.teacher_id"
-	global.GVA_DB.Debug().Raw("select count(*) as total from ("+selectSql+") tb2", 1667318400, 1667404799).Scan(&teacherCensus.Attend)
+	global.GVA_DB.Debug().Raw("select count(*) as total from ("+selectSql+") tb2", startTime, endTime).Scan(&teacherCensus.Attend)
 
 	if err = global.GVA_DB.Model(&model.Teacher{}).Count(&teacherCensus.Num).Error; err != nil {
 		err = errors.New("教师数量获取失败")
@@ -45,7 +47,7 @@ func GetDashboardCensusNum() (err error, carAccess int64, peopleAccess int64, te
 	}
 
 	// 学生考勤
-	if err = global.GVA_DB.Model(&model.StudentAccess{}).Debug().Where("time >=  ? and time <= ? ", 1667318400, 1667404799).Group("student_id").Count(&studentCensus.Attend).Error; err != nil {
+	if err = global.GVA_DB.Model(&model.StudentAccess{}).Debug().Where("time >=  ? and time <= ? ", startTime, endTime).Group("student_id").Count(&studentCensus.Attend).Error; err != nil {
 		err = errors.New("教师考勤数量获取失败")
 		return
 	}
